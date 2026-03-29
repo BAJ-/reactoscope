2d854ce8c6eaaaef29291bbd5d2f02db726919da
Bjørn A. Johansen <baj-@outlook.com>
2026-03-29 16:19:42 +0100

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { schemaPlugin } from './src/observatory/plugins/schemaPlugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), schemaPlugin()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+  },
 })
